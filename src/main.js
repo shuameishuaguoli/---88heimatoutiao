c96e68f01426ae87eda59a5d5011543c0dbae3e5
shuameishuaguoli <shuameishuaguoli@163.com>
2019-11-13 21:15:15 +0800

    黑马头条PC-登录模块-页面布局及样式

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 引入element-ui
+import Element from 'element-ui'
+// 引入样式
+import 'element-ui/lib/theme-chalk/index.css'
 // 引入index.less
 import './style/index.less'
 Vue.config.productionTip = false
+// 在vue中进行注册element
+Vue.use(Element)
 
 new Vue({
   router,
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,42 @@
 <template>
 <!-- 我是登录页 -->
   <div class="login-div">
-      我是登录页
+    <!-- 这里放一个卡片 -->
+      <el-card class="login-card">
+        <div class="login-img">
+          <img src="../../assets/img/logo_index.png" alt="">
+        </div>
+        <el-form style="margin-top:33px">
+            <!-- 手机号输入框 -->
+            <el-form-item >
+              <el-input placeholder="请输入您的手机号"></el-input>
+            </el-form-item>
+            <!-- 手机号输入框 -->
+
+            <!-- 验证码输入框 -->
+            <el-row type="flex" justify="space-between">
+              <el-form-item class="verification" style="width:280px">
+                <el-input placeholder="请输入您的验证码"></el-input>
+              </el-form-item>
+              <el-button style="height:40px">发送验证码</el-button>
+            </el-row>
+            <!-- 验证码输入框 -->
+
+            <!-- 复选框 -->
+            <el-form-item style="margin-top:2px">
+              <el-checkbox>
+                我已阅读并同意用户协议及条款
+              </el-checkbox>
+            </el-form-item>
+            <!-- 复选框 -->
+
+            <!-- 登录按钮 -->
+            <el-form-item style="margin-top:25px">
+               <el-button style="width:100%;" type="primary">登录</el-button>
+            </el-form-item >
+            <!-- 登录按钮 -->
+        </el-form>
+      </el-card>
   </div>
 </template>
 
@@ -24,5 +59,21 @@ export default {
   background-image: url('../../assets/img/login_bg.jpg');
   // 铺满屏幕
   background-size: cover;
+  // 对div进行flex布局
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .login-card{
+    height:360px;
+    width:  440px;
+  }
+  .login-img{
+    // div设置成flex布局
+    display: flex;
+    justify-content: center;
+    img{
+      height: 45px;
+    }
+  }
 }
 </style>
